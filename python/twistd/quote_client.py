964e2388f5bf5b59ad8f87241e5ae9a3fe8850b1
Carl Bäckström <cada@mint>
2013-03-23 10:02:05 +0100

    FEN-0003: Added quote server and client.

---
+++ python/twistd/quote_client.py
@@ -0,0 +1,51 @@
+from twisted.internet import reactor, protocol
+
+class QuoteProtocol(protocol.Protocol):
+	def __init__(self,factory):
+		self.factory = factory
+	
+	def connectionMade(self):
+		self.sendQuote()
+	
+	def sendQuote(self):
+		self.transport.write(self.factory.quote)
+	
+	def dataReceived(self,data):
+		print "Received quote:", data
+		self.transport.loseConnection()
+		
+class QuoteClientFactory(protocol.ClientFactory):
+	def __init__(self,quote):
+		print quote
+		self.quote=quote
+	
+	def buildProtocol(self,addr):
+		return QuoteProtocol(self)
+	
+	def clientConnectionFailed(self, connector, reason):
+		print "connection failed:", reason.getErrorMessage()
+		maybeStopReactor()
+	
+	def clientConnectionLost(self,connector,reason):
+		print "connection lost:", reason.getErrorMessage()
+		maybeStopReactor()
+	
+def maybeStopReactor():
+	global quote_counter
+	quote_counter -= 1
+	if not quote_counter:
+		reactor.stop()
+
+quotes= ["You snooze you loose", "The earlybird gets the worm", "Carpe Diem"]
+quote_counter =len(quotes)
+for quote in quotes:
+	print quote
+	reactor.connectTCP('localhost',8000,QuoteClientFactory(quote))
+reactor.run()
+
+	
+	
+		
+		
+		
+		
