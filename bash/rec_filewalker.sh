74198319577e3fb170f0e05c08545201a95d7394
Carl Backstrom <genzorg@gmail.com>
2013-11-24 12:23:17 +0100

    FEN-0007 Added debug mode

--- bash/rec_filewalker.sh
+++ bash/rec_filewalker.sh
@@ -2,22 +2,23 @@
 
 
 includes=("dox*")
-
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
+DEBUG=0
+
 
 walker ()
 {
-    echo $1
+    [[ -z "$DEBUG" ]] && echo $1
     for file in $1 ; do
         filepath=$(dirname "${file}")
         filename=$(basename "${file}")
-        echo "$file"
+        [[ -z "$DEBUG" ]] && echo "$file"
         if [ -f "$file" ]; then
-            [ ! -L "$2""$filename" ] && [[ "$filename" == *.py ]] && ln -s "$file" "$2""$filename" && echo Symlink "$filename"
+            [ ! -L "$2""$filename" ] && [[ "$filename" == $3 ]] && ln -s "$file" "$2""$filename" && [[ -z "$DEBUG" ]] && echo Symlink "$filename"
         fi
         if [ -d "$file" ] ; then
-            echo "isdir"
+            [[ -z "$DEBUG" ]]&& echo "isdir"
             [ ! -d "$2""$filename" ] && mkdir "$2""$filename" && echo "Madedir"
             walker "$file/*" "$2""$filename"/
         fi
@@ -26,6 +27,6 @@ walker ()
 
 
 
-walker "/home/cada/Desktop/*" "/home/cada/bajstemp/" ""
+#walker "/home/cada/Desktop/*" "/home/cada/bajstemp/" "*.@(py|fy)"
 
 IFS=$SAVEIFS
